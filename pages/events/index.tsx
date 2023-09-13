1c1a5dbbabcf87772586e4f9f2006834312add9f
s-alad <n7.saad@gmail.com>
2023-09-13 19:00:47 -0400

    add events

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import s from "./events.module.scss";
+import Link from "next/link";
 
 export default function Events() {
     return (
         <div>
-            tentative.
+            <div className={s.event}>
+                <h2>first club meeting</h2>
+                <div className={s.sub}>come learn all about BU Blockchain!.</div>
+                <Link href="https://lu.ma/vhseoio4"><button>rsvp</button></Link>
+            </div>
         </div>
     )
 }
