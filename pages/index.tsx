7a172f3d9e93e6f59d1268ef3556dbc633de3f6a
s-alad <n7.saad@gmail.com>
2023-10-10 15:04:17 -0400

    event

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,12 +24,12 @@ const oxygen_mono = Oxygen_Mono({
 export default function Home() {
 
   const eventmap = [
-    {
+/*     {
       name: "Vechain x EasyA Hacks",
       date: "10/07/23",
       action: "rsvp",
       link: "https://www.eventbrite.co.uk/e/vechain-x-easya-hackathon-early-access-7-8-october-win-30000-tickets-705875361207?aff=oddtdtcreator"
-    },
+    }, */
     {
       name: "David Berlind, Editor-in-Chief at Blockchain Journal",
       date: "10/11/23 - CDS B62",
