819fcd8a5b9ec6d0791f5c67fc5fec4534506807
Russell Joarder <proxydox3@gmail.com>
2026-01-23 17:27:58 -0500

    updated events

--- client/data/events.ts
+++ client/data/events.ts
@@ -11,31 +11,30 @@ export interface Event {
 
 export const events: Event[] = [
   {
-    title: "BU Blockchain Hackathon & Pitchathon",
-    date: "November 16, 2025",
-    time: "9:00 AM - 6:00 PM",
-    location: "PHO 906",
-    description: "Join us for a day of hacking and pitching to compete for prizes. Food and drinks will be provided.",
-    callout: "Beginner Friendly!",
-    url: "https://luma.com/qc079my8?tk=jSiJvg",
-    startTimeUtc: "2025-11-16T09:00:00.000Z", // 9:00 AM EDT = 1:00 PM UTC
+    title: "Splash 2.0 Club Fair",
+    date: "January 24, 2026",
+    time: "11:30 AM",
+    location: "GSU Ballroom",
+    description: "Come and say hi to learn more about the club!",
+    url: "https://bublockchain.com",
+    startTimeUtc: "2026-01-24T16:30:00.000Z", // 11:30 AM EST = 4:30 PM UTC
   },
   {
-    title: "Guest Speaker: DeFi Innovation",
-    date: "November 20, 2025",
-    time: "6:30 PM - 7:30 PM",
-    location: "CDS 264",
-    description: "Industry expert discusses the latest trends in decentralized finance.",
-    url: "https://example.com/defi-speaker-event",
-    startTimeUtc: "2025-11-20T22:30:00.000Z", // 6:30 PM EDT = 10:30 PM UTC
+    title: "First Meeting of Spring Semester",
+    date: "January 23, 2026",
+    time: "6:30 PM",
+    location: "CDS 262",
+    description: "Everyone is welcome to come!",
+    url: "https://bublockchain.com",
+    startTimeUtc: "2026-01-23T23:30:00.000Z", // 6:30 PM EST = 11:30 PM UTC
   },
   {
-    title: "Midwest Blockchain Conference",
-    date: "December 5-6, 2025",
-    time: "All Day",
-    location: "Ann Arbor, MI",
-    description: "Attend the Midwest Blockchain Conference to learn about the latest trends in blockchain and network with industry professionals.",
-    url: "https://midwestblockchain.org/",
-    startTimeUtc: "2025-12-05T09:00:00.000Z", // 9:00 AM EDT = 6:00 PM UTC
+    title: "General Meeting",
+    date: "February 4, 2026",
+    time: "6:30 PM",
+    location: "CDS 262",
+    description: "General Meeting",
+    url: "https://bublockchain.com",
+    startTimeUtc: "2026-02-04T23:30:00.000Z", // 6:30 PM EST = 11:30 PM UTC
   },
 ]
